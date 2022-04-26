2cb6e16fc54ee71acf4bfaa86bf04440c8ff5735
George Erokhin <Georgii070684@gmail.com>
2022-04-26 22:51:28 +0300

    add solve two first task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,11 +17,18 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+ 
+  if (isNaN(parseFloat(sampleActivity)) || arguments.length === 0 || sampleActivity > MODERN_ACTIVITY|| sampleActivity <= 0||typeof sampleActivity != "string") {
+    return false
+  }  else {
+    // console.log(1)
+
+ return Math.ceil((Math.log(MODERN_ACTIVITY / (parseFloat(sampleActivity))) )/ (0.693 / HALF_LIFE_PERIOD))
+  }
 }
 
+dateSample('1')
 module.exports = {
   dateSample
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -14,11 +14,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function countCats(Array) {
+  let number = 0;
+  for (let i = 0; i < Array.length; i++) {
+    for (let j = 0; j < Array[i].length; j++) {
+      if (Array[i][j] === '^^') {
+        number++
+      }
+    }
+  }
+  return number
+
   // remove line with error and write your code here
 }
-
+// countCats([
+//   [0, 1, 5],
+//   [0, 7, 5],
+//   [3, 1, 2]
+// ])
 module.exports = {
   countCats
 };
